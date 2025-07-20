be32f6622cfca356a89d8ad9b90b17ec687ac39f
Evdokimova Daria <eudokimovaaa@gmail.com>
2025-07-20 18:48:14 +0300

    Update iam_token_generator.py

--- iam_token_generator.py
+++ iam_token_generator.py
@@ -1,21 +1,24 @@
+import os
 import time
 import json
 from yandexcloud import SDK
 
 class IAMTokenManager:
-    def __init__(self, key_path: str, token_ttl=3600):
-        self.key_path = key_path
+    def __init__(self, sa_key_env_var: str = "SA_KEY", token_ttl=3600):
         self.token_ttl = token_ttl  # Время жизни токена в секундах
         self.iam_token = None
         self.token_created_at = 0
 
-        with open(self.key_path, "r") as f:
-            key_data = json.load(f)
-            self.sdk = SDK(service_account_key=key_data)
+        sa_key_json = os.getenv(sa_key_env_var)
+        if not sa_key_json:
+            raise ValueError(f"Ошибка: переменная окружения {sa_key_env_var} не установлена")
+
+        # Парсим JSON из переменной окружения
+        key_data = json.loads(sa_key_json)
+        self.sdk = SDK(service_account_key=key_data)
 
     def get_iam_token(self):
         now = time.time()
-        # Если токен отсутствует или просрочен, обновляем
         if not self.iam_token or now - self.token_created_at > self.token_ttl:
             credentials = self.sdk.get_credentials()
             self.iam_token = credentials.iam_token
@@ -23,8 +26,9 @@ class IAMTokenManager:
             print("🔄 IAM токен обновлён")
         return self.iam_token
 
-# Пример использования:
-iam_manager = IAMTokenManager("key.json")
 
-# В нужный момент получить актуальный IAM токен:
+# Пример использования
+iam_manager = IAMTokenManager()
+
+# Получаем актуальный IAM токен
 IAM_TOKEN = iam_manager.get_iam_token()
